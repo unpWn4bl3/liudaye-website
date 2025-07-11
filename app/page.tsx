88f2bb8e5b52a02f9590c9fb58a4fb80b953619f
unpWn4bl3 <smiled1999@gmail.com>
2025-07-11 09:59:35 +0800

    test supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+'use client'
+import NewTodo from "@/components/NewTodo";
+import supabase from "@/utils/supabase";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [todos, setTodos] = useState([])
+
+  const fetchTodos = async () => {
+    const { data } = await supabase.from('todos').select('*')
+    setTodos(data)
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div>
+          <NewTodo reload={fetchTodos} />
+          {todos.map((todo) => (
+            <p key={todo.id}>{todo.title}</p>
+          ))}
+        </div>
+
         <Image
           className="dark:invert"
           src="/next.svg"
---
+++ components/NewTodo.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useState } from 'react'
+import supabase from '../utils/supabase'
+
+export default ({ reload }) => {
+    const [title, setTitle] = useState('')
+
+    const addTodo = async (e) => {
+        e.preventDefault()
+        await supabase.from('todos').insert({ title })
+        reload()
+        setTitle('')
+    }
+
+    return (
+        <form onSubmit={addTodo} >
+            <input value={title} onChange={(e) => setTitle(e.target.value)
+            } />
+        </form>
+    )
+}
